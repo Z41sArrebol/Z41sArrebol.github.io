bcf9d86e0bb2ec73e46297f0c8dd539b3eb82c64
Z41sArrebol <3402205659@qq.com>
2024-12-07 05:01:40 +0800

    Site updated: 2024-12-07 05:01:37

--- js/txmap.js
+++ js/txmap.js
@@ -95,7 +95,7 @@ function showWelcome() {
                 case "江苏省":
                     switch (ipLoacation.result.ad_info.city) {
                         case "南京市":
-                            posdesc = "看来我们离得还挺近嗷！";
+                            posdesc = "这是我喜欢的城市啦。";
                             break;
                         case "苏州市":
                             posdesc = "上有天堂，下有苏杭。";
@@ -206,18 +206,18 @@ function showWelcome() {
     //根据本地时间切换欢迎语
     let timeChange;
     let date = new Date();
-    if (date.getHours() >= 5 && date.getHours() < 11) timeChange = "<span>上午好</span>，一日之计在于晨！";
-    else if (date.getHours() >= 11 && date.getHours() < 13) timeChange = "<span>中午好</span>，该摸鱼吃午饭了。";
-    else if (date.getHours() >= 13 && date.getHours() < 15) timeChange = "<span>下午好</span>，懒懒地睡个午觉吧！";
-    else if (date.getHours() >= 15 && date.getHours() < 16) timeChange = "<span>三点几啦</span>，一起饮茶呀！";
+    if (date.getHours() >= 5 && date.getHours() < 11) timeChange = "<span>上午好！！</span>，一日之计在于晨！";
+    else if (date.getHours() >= 11 && date.getHours() < 13) timeChange = "<span>中午好！！</span>，该摸鱼吃午饭了。";
+    else if (date.getHours() >= 13 && date.getHours() < 15) timeChange = "<span>下午好！！</span>，懒懒地睡个午觉吧！";
+    else if (date.getHours() >= 15 && date.getHours() < 16) timeChange = "<span>三点几啦！！</span>，一起饮茶呀！";
     else if (date.getHours() >= 16 && date.getHours() < 19) timeChange = "<span>夕阳无限好！</span>";
-    else if (date.getHours() >= 19 && date.getHours() < 24) timeChange = "<span>晚上好</span>，夜生活嗨起来！";
-    else timeChange = "夜深了，早点休息，少熬夜。";
+    else if (date.getHours() >= 19 && date.getHours() < 24) timeChange = "<span>晚上好！！</span>，夜生活嗨起来！";
+    else timeChange = "夜深了，早点休息，少熬夜噢。";
 
     try {
         //自定义文本和需要放的位置
         document.getElementById("welcome-info").innerHTML =
-            `<b><center>🎉 欢迎信息 🎉</center>&emsp;&emsp;欢迎来自 <span style="color:var(--theme-color)">${pos}</span> 的小伙伴，${timeChange}您现在距离站长约 <span style="color:var(--theme-color)">${dist}</span> 公里，您当前的IP地址为（可能为ipv4/6格式）： <span style="color:var(--theme-color)">${ip}</span>， ${posdesc}</b>`;
+            `<b><center>🎉 欢迎信息 🎉</center></b>`+`<b>&emsp;&emsp;欢迎来自 <span style="color:var(--theme-color)">${pos}</span>的小伙伴，${timeChange}您现在距离站长约<span style="color:var(--theme-color)">${dist}</span>公里，您当前的IP地址为： <span style="color:var(--theme-color)">${ip}</span>， ${posdesc}</b>`;
     } catch (err) {
         // console.log("Pjax无法获取#welcome-info元素🙄🙄🙄")
     }
